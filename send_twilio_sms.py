8fd98cb342290e4e8596e68f4784c7df68cfd3ba
Mark Saville <mark.saville@outlook.com>
2020-11-25 20:14:08 +0100

    Add more descriptive comments

--- send_twilio_sms.py
+++ send_twilio_sms.py
@@ -6,8 +6,8 @@ auth_token = ""
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="",    # your number
-    from_="",  # twilio number
-    body="")
+    to="",    # insert destination number
+    from_="",  # insert twilio number
+    body="") # insert message
 
 print (message.sid)
