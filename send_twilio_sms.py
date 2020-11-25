676f92bab79d1167224f83fd6405eb9358824381
Mark Saville <mark.saville@outlook.com>
2020-11-25 20:11:48 +0100

    Create send and receive user input

--- send_twilio_sms.py
+++ send_twilio_sms.py
@@ -5,4 +5,9 @@ account_sid = ""
 auth_token = ""
 client = Client(account_sid, auth_token)
 
+message = client.messages.create(
+    to="",    # your number
+    from_="",  # twilio number
+    body="")
 
+print (message.sid)
